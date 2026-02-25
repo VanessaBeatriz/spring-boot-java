3888ab4df05e665da4cf43bfcaaaf4a7b2939c2e
unknown <vanessabeatrame@gmail.com>
2026-02-24 22:14:21 -0300

    cotroller criada

---
+++ escola/src/main/java/com/vb/escola/controllers/AlunoController.java
@@ -0,0 +1,32 @@
+package com.vb.escola.controllers;
+
+import com.vb.escola.models.AlunoModel;
+import com.vb.escola.services.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/alunos")
+public class AlunoController {
+
+    @Autowired
+    private AlunoService alunoService;
+
+    @PostMapping
+    public AlunoModel criarAluno(@RequestBody AlunoModel alunoModel){
+        return alunoService.criaAluno(alunoModel);
+    }
+
+    @GetMapping
+    public List<AlunoModel> buscarTodosAlunos(){
+        return alunoService.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarAluno(@PathVariable Long id){
+        alunoService.deletarAluno(id);
+    }
+
+}
